1257067740d920b822cb5cf9d0d31c85cab624ec
Igordl <igor.lucena@ccc.ufcg.edu.br>
2016-08-09 10:09:15 -0300

    Modifiquei getLucroTotal e a visibilidade das estadias

--- src/hotel/Recepcao.java
+++ src/hotel/Recepcao.java
@@ -5,39 +5,43 @@ import java.util.List;
 
 public class Recepcao {
 	final String FIM_DE_LINHA = System.lineSeparator();
-	List<Estadias> estadias;
+	private List<Estadia> estadias;
+
 	public Recepcao() {
-		estadias = new ArrayList<Estadias>();
+		estadias = new ArrayList<Estadia>();
 	}
-	
-	public void checkIn(String nome, String tipo, int idade, int dias, double valor){
-		Estadia estadia = new Estadia(nome,tipo,idade,dias,valor);
+
+	public void checkIn(String nome, String tipo, int idade, int dias, double valor) throws Exception {
+		Estadia estadia = new Estadia(nome, tipo, idade, dias, valor);
 		estadias.add(estadia);
 	}
-	public void checkOut(String nome){
-		for (Estadias estadia : estadias) {
-			if(estadia.getNome().equalsIgnoreCase(nome)){
+
+	public void checkOut(String nome) {
+		for (Estadia estadia : estadias) {
+			if (estadia.getNome().equalsIgnoreCase(nome)) {
 				estadias.remove(estadia);
 			}
-			
+
 		}
 	}
-	public int getNumDeHospedes(){
+
+	public int getNumDeHospedes() {
 		return estadias.size();
 	}
-	public double getLucroTotal(){
+
+	public double getLucroTotal() {
 		double lucroTotal = 0.0;
-		for (Estadias estadia : estadias) {
-			resultado += estadia.getDias() + estadia.getValor();
+		for (Estadia estadia : estadias) {
+			lucroTotal += estadia.getValor();
 		}
 		return lucroTotal;
 	}
-	
-	public String toString(){
-		String retorno = "Estadias:" + FIM_DE_LINHA ;
-		for (Estadias estadia : estadias) {
-			retorno += getNome() + " (" + getTipo() + "): " + getDias() + " dias com o preço de R$ " + getValor() + FIM_DE_LINHA;
-			
+
+	public String toString() {
+		String retorno = "Estadias:" + FIM_DE_LINHA;
+		for (Estadia estadia : estadias) {
+			retorno += estadia.toString();
+
 		}
 		return retorno;
 	}
